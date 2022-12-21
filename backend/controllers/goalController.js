6c63fd05117b25cbdfd030717f3d0025bd3b16d3
ayhcho <cho.anthony12@gmail.com>
2022-12-21 00:41:59 -0800

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,16 @@
-const asyncHandler = require('asyncHandler');
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalModel');
+
 const goalController = {};
 
 // @ desc Get goals
 // @route GET /api/goals
 // @access Private
 goalController.getGoals = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: 'Get goals' });
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
 });
 
 // @ desc Set goal
@@ -17,14 +22,29 @@ goalController.setGoal = asyncHandler(async (req, res, next) => {
       throw new Error('Please add a text field');
     }
 
-    res.status(200).json({ message: req.body });
+    const goal = await Goal.create({
+        text: req.body.text
+    });
+
+    res.status(200).json(goal);
 });
 
 // @ desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
 goalController.updateGoal = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
+        new: true 
+    });
+
+    res.status(200).json(updatedGoal);
 });
 
 
@@ -32,7 +52,16 @@ goalController.updateGoal = asyncHandler(async (req, res, next) => {
 // @route DELETE /api/goals/:id
 // @access Private
 goalController.deleteGoal = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id });
 });
 
 
